efaaf646029484b22843285531aa84f89e95fb83
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-11-25 18:26:09 -0300

    renderiza a lista de planetas

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Mercúrio from '../images/Mercurio.png';
+import Vênus from '../images/Venus.png';
+import Terra from '../images/Terra.png';
+import Marte from '../images/Marte.png';
+import Júpiter from '../images/Jupiter.png';
+import Saturno from '../images/Saturno.png';
+import Urano from '../images/Urano.png';
+import Netuno from '../images/Netuno.png';
 
 class SolarSystem extends React.Component {
   render() {
+    const namePlanets = [
+      'Mercúrio', 'Vênus', 'Terra', 'Marte', 'Júpiter', 'Saturno', 'Urano', 'Netuno',
+    ];
+    const images = [Mercúrio, Vênus, Terra, Marte, Júpiter, Saturno, Urano, Netuno];
+
+    const planets = namePlanets.map((planet, index) => ( // precisa desses parênteses por conta do retorno do HTML
+      <li key={ planet }>
+        <PlanetCard planetName={ planet } planetImage={ images[index] } />
+      </li>));
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <ul>
+          {planets}
+        </ul>
       </div>
     );
   }
