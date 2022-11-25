6bbdf0cdb5123c62f70a1a3e36660f42e98805e4
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-11-25 17:11:16 -0300

    cria o componente PlanetCard

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">{ planetName }</p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string,
+};
+
+PlanetCard.defaultProps = {
+  planetImage: 'imagem do planeta',
+};
+
+export default PlanetCard;
