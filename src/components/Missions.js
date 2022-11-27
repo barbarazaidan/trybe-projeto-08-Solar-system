8f04696e38b50ab5ca4becf603347eb27e036d6c
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-11-27 15:40:01 -0300

    renderiza lista com as missões espaciais

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
 
 class Missions extends React.Component {
   render() {
+    const listOfMissions = missions.map(({ name, year, country, destination }) => ( // todas as info abaixo estão em uma única linha, por isso não usei {}, só dei as quebras para i lint não reclamar de tamanho e ficar mais legível
+      <li key={ name }>
+        <MissionCard
+          name={ name }
+          year={ year }
+          country={ country }
+          destination={ destination }
+        />
+      </li>));
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <ul>
+          {listOfMissions}
+        </ul>
       </div>
     );
   }
